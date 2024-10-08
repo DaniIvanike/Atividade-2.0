9054635fdd18ae7f8d85a36bb5e1939e50fb506b
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-10-08 10:44:27 -0300

    .

---
+++ main.js
@@ -0,0 +1,9 @@
+function mostrsResposta (cartao_conteudo){
+    const resposta = cartao_conteudo.querySelector('.cartao_conteudo_resposta');
+    if (resposta.style.display) == 'none'){
+        resposta.style.display == 'block';
+    }
+    else{
+        resposta.style.display == 'none';
+    }
+}
